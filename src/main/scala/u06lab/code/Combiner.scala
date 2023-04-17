fb2dbc1573538e8a9ebfd52ce94d241a3dc21fbb
Sofy24 <sofia.tosi4@studio.unibo.it>
2023-04-18 00:29:37 +0200

    Ex 1

--- src/main/scala/u06lab/code/Combiner.scala
+++ src/main/scala/u06lab/code/Combiner.scala
@@ -1,4 +1,5 @@
 package u06lab.code
+import scala.annotation.tailrec
 
 /** 1) Implement trait Functions with an object FunctionsImpl such that the code in TryFunctions works correctly. */
 
@@ -6,11 +7,29 @@ trait Functions:
   def sum(a: List[Double]): Double
   def concat(a: Seq[String]): String
   def max(a: List[Int]): Int // gives Int.MinValue if a is empty
+  def combineElements[T](e: T, elems: T*)(using combiner: Combiner[T]): T
 
 object FunctionsImpl extends Functions:
-  override def sum(a: List[Double]): Double = ???
-  override def concat(a: Seq[String]): String = ???
-  override def max(a: List[Int]): Int = ???
+  override def sum(a: List[Double]): Double = a.sum
+  override def concat(a: Seq[String]): String =
+    if a.nonEmpty then a.reduce( _ ++ _)
+    else ""
+  override def max(a: List[Int]): Int =
+    if a.nonEmpty then a.max
+    else Int.MinValue
+  @tailrec
+  override def combineElements [T: Combiner](elem: T, elems: T*): T =
+    if elems.isEmpty then
+      summon[Combiner[T]].unit
+    else
+      if elems.tail.nonEmpty then
+        combineElements(summon[Combiner[T]].combine(elem, elems.head), elems.tail.head)
+      else
+        summon[Combiner[T]].combine(elem, elems.head)
+
+
+
+
 
 /*
  * 2) To apply DRY principle at the best,
@@ -29,6 +48,21 @@ trait Combiner[A]:
   def unit: A
   def combine(a: A, b: A): A
 
+object GivenCombiners:
+  given Combiner[Double] with
+    override def combine(a: Double, b: Double): Double = a + b
+    override def unit: Double = 0.0
+  given Combiner[String] with
+    override def combine(a: String, b: String): String = a ++ b
+    override def unit: String = ""
+  given Combiner[Int] with
+    override def combine(a: Int, b: Int): Int = if a > b then a else b
+    override def unit: Int = Int.MinValue
+
+import GivenCombiners.given
+
+
+
 @main def checkFunctions(): Unit =
   val f: Functions = FunctionsImpl
   println(f.sum(List(10.0, 20.0, 30.1))) // 60.1
@@ -37,3 +71,10 @@ trait Combiner[A]:
   println(f.concat(Seq())) // ""
   println(f.max(List(-10, 3, -5, 0))) // 3
   println(f.max(List())) // -2147483648
+
+  println(f.combineElements(10.0, 20.0, 30.1)) // 60.1
+  println(f.combineElements(0.0)) // 0.0
+  println(f.combineElements("a", "b", "c")) // abc
+  println(f.combineElements("")) // ""
+  println(f.combineElements(-10, 3, -5, 0)) // 3
+  println(f.combineElements(0)) // -2147483648
