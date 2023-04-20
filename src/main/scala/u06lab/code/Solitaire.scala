f9358e394c87d1ea1f692ba12736898b71749d55
Sofy24 <sofia.tosi4@studio.unibo.it>
2023-04-20 19:09:53 +0200

    Ex 2

--- src/main/scala/u06lab/code/Solitaire.scala
+++ src/main/scala/u06lab/code/Solitaire.scala
@@ -1,6 +1,11 @@
 package u06lab.code
 
 object Solitaire extends App:
+  type Cell = (Int, Int)
+  type Solution = Iterable[Cell]
+  type IterableFactory = Solution => Iterable[Solution]
+  val width = 5
+  val height = 5
   def render(solution: Seq[(Int, Int)], width: Int, height: Int): String =
     val reversed = solution.reverse
     val rows =
@@ -11,5 +16,9 @@ object Solitaire extends App:
       yield row.mkString
     rows.mkString("\n")
 
+  //def putFirstPosition():
 
-  println(render(solution = Seq((0, 0), (2, 1)), width = 3, height = 3))
+
+  //println(render(solution = Seq((0, 0), (2, 1)), width = 3, height = 3))
+
+  println(render(solution = Seq((0, 0), (2, 1)), width = 5, height = 5))
